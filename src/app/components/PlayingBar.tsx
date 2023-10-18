343844da8d946164d3cb04208369b00e8cd7e03b
Joshua <joshuaxdmb@gmail.com>
2023-10-18 08:54:30 -0400

    Player before implementing classes WIP

--- src/app/components/PlayingBar.tsx
+++ src/app/components/PlayingBar.tsx
@@ -4,6 +4,7 @@ import { useRecoilState } from 'recoil';
 import { isPlayingAtom, currentTrackAtom } from '@/atoms/playingSongAtom';
 import { useSpotify } from '@/hooks/useSpotify';
 import { SongLocal } from '@/types/types';
+import { NonPremiumPlayer, Player, PremiumPlayer } from '@/classes/PlayerClass';
 
 type Props = {};
 
@@ -13,16 +14,27 @@ const PlayingBar: React.FC<Props> = () => {
   const [audio, setAudio] = useState<HTMLAudioElement>();
   const [isUserPremium, setIsUserPremium] = useState(false);
   const { userDetails, spotifyApi, spotifyDeviceId } = useSpotify();
+  const [volume, setVolume] = useState(70);
+  const [player, setPlayer] = useState<Player | undefined>();
 
   useEffect(() => {
-    spotifyApi.transferMyPlayback([spotifyDeviceId]).catch((e) => {
-      console.log('Error setting device on Spotify', e);
-    });
-
     if (userDetails?.product === 'premium') {
       setIsUserPremium(true);
+      spotifyApi.transferMyPlayback([spotifyDeviceId]).catch((e) => {
+        console.log('Error setting device on Spotify', e);
+      });
+      setPlayer(new PremiumPlayer(spotifyApi, spotifyDeviceId));
+    } else {
+      setPlayer(new NonPremiumPlayer());
     }
   }, [spotifyDeviceId, spotifyApi, userDetails?.product]);
+
+  useEffect(() => { //autoplay when currentTrack changes
+    if (currentTrack?.spotify_id && player) {
+      player.play(currentTrack);
+      setIsPlaying(true);
+    }
+  }, [currentTrack]);// eslint-disable-line
 
   const playSong = async (song: SongLocal) => {
     if (isUserPremium) {
@@ -56,16 +68,13 @@ const PlayingBar: React.FC<Props> = () => {
     };
   };
 
-  useEffect(() => {
-    if (currentTrack?.spotify_id) playSong(currentTrack);
-  }, [currentTrack]);
-
   const handlePlayPause = () => {
-    if (isUserPremium) {
-      isPlaying ? spotifyApi.pause() : spotifyApi.play();
-    }
-    if (audio) {
-      isPlaying ? audio.pause() : audio.play();
+    if (isPlaying) {
+      player?.pause();
+      setIsPlaying(false);
+    } else {
+      player?.play(currentTrack!); // Assuming there's always a track to play
+      setIsPlaying(true);
     }
   };
 
---
+++ src/classes/PlayerClass.ts
@@ -0,0 +1,58 @@
+import { SongLocal } from "@/types/types";
+
+export abstract class Player {
+    abstract play(song: SongLocal): void;
+    abstract pause(): void;
+    abstract stop(): void;
+  }
+  
+  // 2. Derived PremiumPlayer class
+  export class PremiumPlayer extends Player {
+    constructor(private spotifyApi: any, private spotifyDeviceId: string) {
+      super();
+    }
+  
+    play(song: SongLocal): void {
+      console.log('Currently playing', song?.title);
+      this.spotifyApi
+        .transferMyPlayback([this.spotifyDeviceId])
+        .then(() => {
+          this.spotifyApi
+            .play({ uris: [`spotify:track:${song.spotify_id}`] })
+            .catch((err: any) => {
+              console.log('Something failed playing from Spotify', err);
+            });
+        })
+        .catch((e:any) => {
+          console.log('Error setting device on Spotify', e);
+        });
+    }
+  
+    pause(): void {
+      this.spotifyApi.pause();
+    }
+  
+    stop(): void {
+      this.spotifyApi.pause(); // Assuming the stop behavior for the premium user is the same as pause
+    }
+  }
+  
+  // 2. Derived NonPremiumPlayer class
+  export class NonPremiumPlayer extends Player {
+    private audioInstance?: HTMLAudioElement;
+  
+    play(song: SongLocal): void {
+      this.audioInstance = new Audio(song?.preview_url || '');
+      this.audioInstance.play();
+    }
+  
+    pause(): void {
+      this.audioInstance?.pause();
+    }
+  
+    stop(): void {
+      this.audioInstance?.pause();
+      this.audioInstance = undefined; // Cleanup the instance if you want to mimic a stop behavior
+    }
+  }
+  
