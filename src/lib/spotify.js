799221ef3ed2a7d766158d612d945a4bac697c81
Joshua <joshuaxdmb@gmail.com>
2023-12-16 17:23:08 -0500

    modify spotify scopes

--- src/lib/spotify.js
+++ src/lib/spotify.js
@@ -3,19 +3,11 @@ import { getUrl } from "./helpers"
 
 const scopes = [
     'user-read-email',
-    'playlist-read-private',
-    'playlist-read-collaborative',
-    'user-read-email',
     'streaming',
     'user-read-private',
-    'user-library-read',
-    'user-library-modify',
-    'user-top-read',
     'user-read-playback-state',
     'user-modify-playback-state',
     'user-read-currently-playing',
-    'user-read-recently-played',
-    'user-follow-read',
     'user-read-private',
 ].join(',')
 
