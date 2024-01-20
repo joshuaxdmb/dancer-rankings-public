41ad162edb6df4e3f7b01a56ec1ed222a48d139f
Joshua <joshuaxdmb@gmail.com>
2024-01-20 13:12:34 -0500

    feat(create-event): DRY add class component

--- src/app/create-event/components/CreateEventDetails.tsx
+++ src/app/create-event/components/CreateEventDetails.tsx
@@ -215,39 +215,6 @@ const CreateEventDetails = ({}: Props) => {
             setClassesIncluded={setClassesIncluded}
             startTime={startTime}
             />
-            // <div>
-            //   {classesIncluded.map((c, i) => {
-            //     return (
-            //       <ClassInputFielt
-            //         key={i}
-            //         index={i}
-            //         eventClass={c}
-            //         setClass={updateClassesIncluded}
-            //         editable={editable}
-            //       />
-            //     )
-            //   })}
-            //   <StyledButton
-            //     onClick={addClass}
-            //     style={{
-            //       backgroundColor: 'rgba(255,255,255,0.45)',
-            //       color: 'white',
-            //       fontWeight: 'lighter',
-            //     }}
-            //     className={`rounded-lg sm:min-w-[260px] h-10 text-left items-center justify-start pl-3 text-sm mt-4`}>
-            //     {<HiMiniPlusCircle size={20} />} Add Class
-            //   </StyledButton>
-            //   <StyledButton
-            //     onClick={removeLastClass}
-            //     style={{
-            //       backgroundColor: 'rgba(255,255,255,0.45)',
-            //       color: 'white',
-            //       fontWeight: 'lighter',
-            //     }}
-            //     className={`rounded-lg sm:min-w-[260px] h-10 text-left items-center justify-start pl-3 text-sm mt-4`}>
-            //     {<HiMiniMinusCircle size={20} />} Remove Last
-            //   </StyledButton>
-            // </div>
           ) : (
             <StyledButton
               onClick={addClass}
